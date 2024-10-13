9cb672735354ceed8c8f03b19177c6978194d73d
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-10-13 18:45:26 +0000

    Add REPL basics

---
+++ 01b_scripting with_python_pep_get_help/hello_network.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+/usr/bin/env python -c "print('Hello, Network!')"
+
+echo ""
+
+/usr/bin/env python --version
+
+echo -e "\nPress Ctrl+D to exit the Python REPL when done\n"
+
+/usr/bin/env python
