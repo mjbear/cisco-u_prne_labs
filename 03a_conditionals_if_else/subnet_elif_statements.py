2767fc1b9c5f0b3350661d75cf47fdf15a60ae8f
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-10-13 21:14:33 +0000

    Add section3, lab1 (03a) initial files

---
+++ 03a_conditionals_if_else/subnet_elif_statements.py
@@ -0,0 +1,14 @@
+elif subnet.endswith('.128'):
+    subnet_slash_notation = '/25'
+elif subnet.endswith('.192'):
+    subnet_slash_notation = '/26'
+elif subnet.endswith('.224'):
+    subnet_slash_notation = '/27'
+elif subnet.endswith('.240'):
+    subnet_slash_notation = '/28'
+elif subnet.endswith('.248'):
+    subnet_slash_notation = '/29'
+elif subnet.endswith('.252'):
+    subnet_slash_notation = '/30'
+elif subnet.endswith('.255'):
+    subnet_slash_notation = '/32'
