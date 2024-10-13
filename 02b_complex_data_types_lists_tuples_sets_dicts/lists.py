76cac7f6d2b2d5f5980fd3b7c2b3124e0edb53d6
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-10-13 20:08:06 +0000

    lab 02b - lists

--- 02b_complex_data_types_lists_tuples_sets_dicts/lists.py
+++ 02b_complex_data_types_lists_tuples_sets_dicts/lists.py
@@ -1,11 +1,24 @@
 import netmiko
 
+ip_list = [
+    '10.254.0.1',
+    '10.254.0.2',
+    '10.254.0.3',
+]
+# print(ip_list[0])
+# print(ip_list[-1])
+
 username = 'cisco' 
 password = 'cisco' 
 device_type = 'cisco_ios' 
 port = 22 
-    
+
+output_list = []
 for ip in ip_list:
     net_connect = netmiko.ConnectHandler(ip=ip, device_type=device_type, 
         username=username, password=password, port=port)
     output = net_connect.send_command('show ip interface brief')
+    output_list.append(output)
+
+for output in output_list:
+    print(output)
