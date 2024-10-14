04dfaf11f2323aeffa003534fd41879cd5c2f940
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-10-14 18:23:18 +0000

    Add lab 06a - network_audit - func with variables

--- 06a_code_reuse_functions/ciscoIOS.py
+++ 06a_code_reuse_functions/ciscoIOS.py
@@ -1,3 +1,6 @@
+import netmiko
+
+
 # ip_list = []
 
 def populate_ip_list(ip_base, ip_range):
@@ -7,5 +10,16 @@ def populate_ip_list(ip_base, ip_range):
         ip_list.append(ip_addr)
     return ip_list
 
-ip_list = populate_ip_list('10.0.0.', range(1,33))
-print(ip_list)
+# ip_list = populate_ip_list('10.0.0.', range(1,33))
+# print(ip_list)
+
+
+def get_ios_output(ip_list, command):
+    for ip in ip_list:
+        net_connect = netmiko.ConnectHandler(ip=ip, device_type='cisco_ios',
+            username='cisco', password='cisco', port=22)
+        print(net_connect.send_command(command))
+
+
+ip_list = populate_ip_list('10.254.0.', range(1,4))
+get_ios_output(ip_list, 'sh ip int br')
