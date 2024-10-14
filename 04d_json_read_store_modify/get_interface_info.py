54aad8bf6a46e884abf1f95062c6dc7e4403456c
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-10-14 01:07:28 +0000

    Add lab 04d - manipulate json

--- 04d_json_read_store_modify/get_interface_info.py
+++ 04d_json_read_store_modify/get_interface_info.py
@@ -4,4 +4,13 @@ import pprint
 with open('interface_info.json') as json_file:
     json_data = json.loads(json_file.read())
 
+    # pprint.pprint(json_data)
+    
+    # create pointer variable
+    interface_list = json_data['Cisco-IOS-XE-native:interface']['GigabitEthernet']
+    for intf in interface_list:
+        if 'ip' not in intf:
+            interface_list.remove(intf)
+
+    # has only 2-4 since we removed number GigabitEthernet1
     pprint.pprint(json_data)
