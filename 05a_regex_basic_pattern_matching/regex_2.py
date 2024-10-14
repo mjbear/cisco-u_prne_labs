6a1f8ac335f5d8b1972f9a517f7579dd1175f49c
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-10-14 03:29:14 +0000

    Add lab 05a - regex - extract specific values

--- 05a_regex_basic_pattern_matching/regex_2.py
+++ 05a_regex_basic_pattern_matching/regex_2.py
@@ -10,3 +10,13 @@ port = '22'
 net_connect = netmiko.ConnectHandler(ip=ip, device_type=device_type,
     username=username, password=password, port=port)
 csr1kv1_ip_int_br = net_connect.send_command('show ip interface brief')
+
+# re.search only finds the first match
+##
+# ip_addr = re.search(r'\w+\s+([\d\.]+)', csr1kv1_ip_int_br)
+# print(ip_addr.group(1))
+
+# re.findall finds all matches
+##
+ip_addr = re.findall(r'\w+\s+([\d\.]+)', csr1kv1_ip_int_br)
+print(ip_addr)
