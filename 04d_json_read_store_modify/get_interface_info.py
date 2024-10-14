5fca44080464ad0b3de96b892f510b85fd305598
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-10-14 01:10:03 +0000

    Add lab 04d - store json

--- 04d_json_read_store_modify/get_interface_info.py
+++ 04d_json_read_store_modify/get_interface_info.py
@@ -1,5 +1,6 @@
 import json
 import pprint
+from textwrap import indent
 
 with open('interface_info.json') as json_file:
     json_data = json.loads(json_file.read())
@@ -13,4 +14,8 @@ with open('interface_info.json') as json_file:
             interface_list.remove(intf)
 
     # has only 2-4 since we removed number GigabitEthernet1
-    pprint.pprint(json_data)
+    # pprint.pprint(json_data)
+
+    formatted_json_data = json.dumps(json_data, indent=4)
+    with open('interfaces_with_IPs.json', 'w') as json_file:
+        print(formatted_json_data, file=json_file)
