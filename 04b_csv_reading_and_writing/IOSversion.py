b3f6815bab6b94ca4da2b9d3c7f778f2ecd8480e
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-10-14 00:15:36 +0000

    Add lab 04b - manipulate csv data

--- 04b_csv_reading_and_writing/IOSversion.py
+++ 04b_csv_reading_and_writing/IOSversion.py
@@ -1,9 +1,30 @@
 import pandas
+import cisco
 
 with open('router_info.csv') as csv_file:
     df = pandas.read_csv(csv_file)
-print(df)
-print()
-print(df['ip'])
-print()
-print(df.loc[[0]])
+# print(df)
+# print()
+# print(df['ip'])
+# print()
+# print(df.loc[[0]])
+
+router_list = []
+version_list = []
+
+for index, row in df.iterrows():
+    router = cisco.CiscoIOS(
+        row['ip'],
+        username=row['username'],
+        password=row['password'],
+        port=row['port'],
+        device_type=row['device_type']
+    )
+    version_data = router.get_IOS_version()
+    hostname = version_data['hostname']
+    version = f"{version_data['rommon']} {version_data['version']}"
+
+    router_list.append(hostname)
+    version_list.append(version)
+
+print(router_list, version_list, sep='\n')
