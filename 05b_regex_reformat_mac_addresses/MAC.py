2ec25e13ddfcbe6c7cfeaefbbd9d3895a49029c0
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-10-14 10:43:57 +0000

    Add lab 05b - regex - read and store

--- 05b_regex_reformat_mac_addresses/MAC.py
+++ 05b_regex_reformat_mac_addresses/MAC.py
@@ -11,3 +11,6 @@ net_connect = netmiko.ConnectHandler(ip=ip, device_type=device_type,
     username=username, password=password, port=port)
 show_arp = net_connect.send_command('show arp')
 print(show_arp)
+
+mac_list = re.findall(r'((?:[0-9a-f]{4}\.){2}[0-9a-f]{4})', show_arp)
+print(mac_list)
