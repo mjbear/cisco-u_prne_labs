1f7622c4e2d3a7331380aace6ca49a9a6629f7c9
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-10-14 18:17:25 +0000

    Add lab 06a - func using local var and return

--- 06a_code_reuse_functions/ciscoIOS.py
+++ 06a_code_reuse_functions/ciscoIOS.py
@@ -1,9 +1,11 @@
-ip_list = []
+# ip_list = []
 
 def populate_ip_list(ip_base, ip_range):
+    ip_list = []
     for n in ip_range:
         ip_addr = ip_base + str(n)
         ip_list.append(ip_addr)
+    return ip_list
 
-populate_ip_list('10.0.0.', range(1,33))
+ip_list = populate_ip_list('10.0.0.', range(1,33))
 print(ip_list)
