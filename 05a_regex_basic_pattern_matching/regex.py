2defd4cb31154f0c345e86c092d48722d867655c
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-10-14 03:11:19 +0000

    Add lab 05a - regex - split string

--- 05a_regex_basic_pattern_matching/regex.py
+++ 05a_regex_basic_pattern_matching/regex.py
@@ -10,8 +10,11 @@ port = '22'
 net_connect = netmiko.ConnectHandler(ip=ip, device_type=device_type,
     username=username, password=password, port=port)
 csr1kv1_ip_int_br = net_connect.send_command('sh ip int br')
-print(csr1kv1_ip_int_br)
-print(type(csr1kv1_ip_int_br))
+# print(csr1kv1_ip_int_br)
+# print(type(csr1kv1_ip_int_br))
+
+sh_ip_int_split = re.split(r'\n', csr1kv1_ip_int_br)
+print(sh_ip_int_split)
 
 # regex_pattern = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
 # ip_string = str()
