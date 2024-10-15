cc5d15cf9fe7514cf4e5f6fbef56bcfbe42f0a13
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-10-15 01:41:18 +0000

    Add lab 06e - pip package management

---
+++ 06e_import_external_libs/pip_pkg_mgmt.sh
@@ -0,0 +1,26 @@
+python -c 'import privy'
+
+pip list | grep privy
+
+python -m pip install --upgrade pip
+
+pip install privy
+
+privy_cmd=$(cat <<EOF
+import privy
+encrypted_pw = privy.hide(b'password', b'decrypt')
+print()
+print(encrypted_pw)
+print()
+print(privy.peek(encrypted_pw, b'decrypt'))
+print(privy.peek(encrypted_pw, b'decrypt').decode('utf-8'))
+print()
+enc = privy.hide('cisco'.encode(), 'decrypt'.encode())
+print(enc)
+print()
+print(privy.peek(enc, 'decrypt'.encode()).decode())
+EOF
+)
+
+python -c "$privy_cmd"
+
