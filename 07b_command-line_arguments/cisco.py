c6101cc70fbc94abc6fcac985ba7446938438bca
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-10-16 18:41:11 +0000

    Add lab 07b - argparse for arguments

--- 07b_command-line_arguments/cisco.py
+++ 07b_command-line_arguments/cisco.py
@@ -2,6 +2,7 @@ import netmiko
 import pandas as pd
 from getpass import getpass
 import sys
+import argparse
 
 
 class CiscoIOS():
@@ -49,8 +50,60 @@ class CiscoIOS():
         else:
             return 'Error - Invalid interface name.'
 
+
+class CiscoIOS_CLI(CiscoIOS):
+
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            'ip', 
+            type=str, 
+            help='IP address, SSH endpoint'
+        )
+        parser.add_argument(
+            '--port', 
+            type=int, 
+            default=22, 
+            help='Port for connection'
+        )
+        parser.add_argument(
+            '-u', '--username', 
+            type=str, 
+            help='Username for SSH connection'
+        )
+        parser.add_argument(
+            '-p', '--password', 
+            type=str, 
+            help='Password for SSH connection'
+        )
+        parser.add_argument(
+            '--device_type', 
+            type=str, 
+            default='cisco_ios', 
+            help='Device type of appliance'
+        )
+        
+        args = parser.parse_args()
+
+        ip = args.ip
+        port = args.port
+        username = args.username
+        password = args.password
+        device_type = args.device_type
+        self.conn = netmiko.ConnectHandler(ip=ip, port=port, username=username,
+            password=password, device_type=device_type)
+        print(self.conn.send_command('show run | include hostname'))
+
+    def get_interface_list(self):
+        return super().get_interface_list()
+
+    def get_run_int(self):
+        return super().get_run_int()
+
+
 def main():
-    csr = CiscoIOS(sys.argv[1], username='cisco', password='cisco')
+    # csr = CiscoIOS(sys.argv[1], username='cisco', password='cisco')
+    csr = CiscoIOS_CLI()
     print(csr.get_interface_list())
 
 if __name__ == '__main__':
