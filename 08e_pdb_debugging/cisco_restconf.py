762b03178da7ea698edc498c51ea64eca0747f0d
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-10-19 02:36:31 +0000

    Add lab 08e - debug with pdb.set_trace

---
+++ 08e_pdb_debugging/cisco_restconf.py
@@ -0,0 +1,22 @@
+import json
+import os
+import requests
+import pandas
+
+
+DASH = '-' * os.get_terminal_size().columns
+conn_info = pandas.read_csv('conn_info.csv')
+
+# for index, row in conn_info.iteritems():
+for index, row in conn_info.iterrows():
+    import pdb; pdb.set_trace()
+    ip = row['ip']
+    username = row['username']
+    password = row['password']
+    uri = f'https://{ip}:443/restconf'
+    auth = (username, password)
+
+    # response = requests.get(uri, auth=auth)
+    response = requests.get(uri, auth=auth, verify=False)
+    print(response.json())
+    print(DASH)
